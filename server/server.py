f4eeac0b4cf6f86a31b78ea22077471cd6abdb97
Robert Huang <rh5661@rit.edu>
2023-01-04 17:05:44 -0500

    API Item: Functionality implemented and tested

--- server/server.py
+++ server/server.py
@@ -5,14 +5,16 @@ from flask_cors import CORS
 from src.db.src.db_utils import *
 from src.api.src.users import *
 from src.api.src.user import *
-
+from src.api.src.items import *
+from src.api.src.item import *
 
 app = Flask(__name__) #create Flask instance
 CORS(app) #Enable CORS on Flask server to work with Nodejs pages
 api = Api(app) #api router
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user/<string:user_id>')
-
+api.add_resource(Items, '/items')
+api.add_resource(Item, '/item/<string:item_id>')
 
 if __name__ == '__main__':
     print("Loading db");
--- server/tests/tests_api/test_receipt_calc_api.py
+++ server/tests/tests_api/test_receipt_calc_api.py
@@ -40,13 +40,52 @@ class TestReceiptCalcApi(unittest.TestCase):
         self.assertEqual(actual,expected)
     
     def test_delete_user(self):
-        put_rest_call(self,"http://localhost:5000/user/1")
+        delete_rest_call(self,"http://localhost:5000/user/1")
 
-        res = get_rest_call(self, "http://localhost:5000/user/1")
-        actual = res 
-        expected = [{'id': 1, 'name': None}]
+        user_count = exec_get_one('SELECT COUNT(*) FROM users')
+
+        actual = user_count[0]
+        expected = 2
         self.assertEqual(actual,expected)
 
+    def test_get_item(self):
+        """tests retrieving a item with a GET call
+        """        
+        actual = get_rest_call(self, "http://localhost:5000/item/1")
+        expected = [{'cost_per_user': 12.99,'id': 1,'name': 'Swifter','store': 'Walmart','tax': 8,'total_cost': 12.99}]
+        self.assertEqual(actual,expected)
+    
+    def test_post_item(self):
+        """tests creating a item with a POST call
+        """        
+        data = dict(store = "CVS", name = "Tylenol",  tax = 8, cost = 8.62)
+        jdata = json.dumps(data)
+        hdr = {'content-type': 'application/json'}
+        post_rest_call(self,"http://localhost:5000/items",jdata,hdr)
 
+        res = get_rest_call(self, "http://localhost:5000/item/4")
+        actual = res 
+        expected = [{'cost_per_user': 9.3,'id': 4,'name': 'Tylenol','store': 'CVS','tax': 8,'total_cost': 9.3}]
+        self.assertEqual(actual,expected)
+    
+    def test_put_item(self):
+        data = dict(store = "Target", name = "Potats",  tax = 8, cost = 9.99, cost_per_user = 9.99)
+        jdata = json.dumps(data)
+        hdr = {'content-type': 'application/json'}
+        put_rest_call(self,"http://localhost:5000/item/2",jdata,hdr)
+
+        res = get_rest_call(self, "http://localhost:5000/item/2")
+        actual = res
+        expected = [{'cost_per_user': 9.99,'id': 2,'name': 'Potats','store': 'Target','tax': 8,'total_cost': 9.99}]
+        self.assertEqual(actual,expected)
+    
+    def test_delete_item(self):
+        delete_rest_call(self,"http://localhost:5000/item/1")
+
+        item_count = exec_get_one('SELECT COUNT(*) FROM items')
+        
+        actual = item_count[0]
+        expected = 2
+        self.assertEqual(actual, expected)
 
 
